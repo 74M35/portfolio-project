85c6a083478d904e9d17303ef8f5819ca351220d
James Speller <J@miee.uk>
2021-10-05 22:58:05 +0100

    Added Blog Page, minor home page edits

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,17 @@ class Blog(models.Model):
     pubDate = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+    def pubDateShort(self):
+        date = int(self.pubDate.strftime('%e'))
+        if 4 <= date <= 20 or 24 <= date <= 30:
+            suffix = "th "
+        else:
+            suffix = ["st ", "nd ", "rd "][date % 10 - 1]
+        properDate = self.pubDate.strftime('%B %e') + suffix + self.pubDate.strftime('%Y')
+        return properDate
