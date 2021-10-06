924971bae9343300d63896cec0604d5b37e8cf41
James Speller <J@miee.uk>
2021-10-06 16:15:59 +0100

    Changed homepage and job

--- jobs/models.py
+++ jobs/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to="images/")
     summary = models.CharField(max_length=200)
+    link = models.CharField(max_length=200, default='/')
 
     def __str__(self):
         return self.summary
