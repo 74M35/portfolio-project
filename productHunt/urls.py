e102208b89df55ecaf88e3f69148866cade0013f
James Speller <J@miee.uk>
2021-10-17 16:19:46 +0100

    added in the product hunt clone

--- productHunt/urls.py
+++ productHunt/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='PHhome'),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
